9fb813cdbc875bfd5ac07fee6e34053872b6931a
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-04-16 07:56:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
 // your code here
+	let arr = []
+	for(int i=0;i<num;i++){
+		if(i==0) arr.push(0)
+		else if(i==1) arr.push(1)
+		else arr.push(arr[i-1]+arr[i-2])
+	}
+	return arr
 }
 
 module.exports = fibonacci;
