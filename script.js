e7008a1f596354b60c670711e88c14cadeedc59a
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-04-16 08:36:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function fibonacci(num) {
 // your code here
 	let arr = []
-	for(int i=0;i<num;i++){
+	for(let i=0;i<num;i++){
 		if(i==0) arr.push(0)
 		else if(i==1) arr.push(1)
 		else arr.push(arr[i-1]+arr[i-2])
